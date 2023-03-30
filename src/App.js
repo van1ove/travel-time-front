f04421ebd44de9b9fed6041de6cdaaff2191ae0d
van1ove <berezko2004@gmail.com>
2023-03-30 15:49:10 +0300

    Added map and first interactable area

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
-import './App.css';
+import './CSS/App.css';
+
+import Europe from './Components/Europe';
+import './CSS/Europe.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Europe/>
+      {/* <header className="App-header">
+        { <img src={logo} className="App-logo" alt="logo" /> }
+        
+      </header> */}
     </div>
   );
 }
